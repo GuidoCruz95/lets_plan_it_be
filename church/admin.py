04015ce92260feb751b67891d4839efd7a1edf3e
Guido Cruz <guidocruz@Guidos-MacBook-Pro.local>
2023-02-15 10:32:07 -0400

    basic models for church organization and basic requests defined

--- church/admin.py
+++ church/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
-from .models import Person
+from .models import Person, Cell, Macro
+
+
+class MacroAdmin(admin.ModelAdmin):
+    fields = ['name', 'creation_date', 'closing_date']
+
+
+class CellAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'macro']
 
 
 class PersonAdmin(admin.ModelAdmin):
-    fields = ['ci', 'email', 'name', 'lastname', 'birthdate']
+    fields = ['ci', 'email', 'name', 'lastname', 'birthdate', 'cell']
 
 
+admin.site.register(Macro, MacroAdmin)
+admin.site.register(Cell, CellAdmin)
 admin.site.register(Person, PersonAdmin)
--- church/models.py
+++ church/models.py
@@ -1,11 +1,26 @@
+import uuid
+
 from django.db import models
-import datetime
 
 
-# Create your models here.
+class Macro(models.Model):
+    macro_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=20)
+    creation_date = models.CharField(max_length=100)
+    closing_date = models.CharField(max_length=100)
+
+
+class Cell(models.Model):
+    cell_id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=20)
+    description = models.CharField(max_length=100)
+    macro = models.ForeignKey(Macro, on_delete=models.CASCADE, related_name='cells', null=True)
+
+
 class Person(models.Model):
-    ci = models.IntegerField()
+    ci = models.IntegerField(primary_key=True)
     email = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     lastname = models.CharField(max_length=30)
     birthdate = models.CharField(max_length=30)
+    cell = models.ForeignKey(Cell, on_delete=models.CASCADE, related_name='people', null=True)
